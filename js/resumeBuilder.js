04ecc18deff1c81f5a4cc33adcd9009e268315ec
cdg-cac <carriedgibson@gmail.com>
2018-08-18 21:37:43 -0700

    dshsdh

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,6 +1,3 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
 
 var name = "Carrie Knowlton";
 var role = "Web Developer";
@@ -21,24 +18,41 @@ var bio = {
     "picture URL" : "/images/pic.png",
     "welcome message" : "Welcome!",
     "skills" : ["javascript", "other things"]
-}
+};
 
 var work = {
     "jobs": [
         {
             "employer": "UCLA Extension",
-            "title" = "Front-End Programmer",
-            "dates" = "March 2017 - present",
-            "description" = "lorem ipsum"
+            "title": "Front-End Programmer",
+            "location": "Los Angeles, CA",
+            "dates": "March 2017 - present",
+            "description": "lorem ipsum"
         },
         {
             "employer": "USC Center for Effective Organiations",
-            "title" = "Webmaster",
-            "dates" = "November 2012 - March 2017",
-            "description" = "lorem ipsum"
+            "title": "Webmaster",
+            "location": "Los Angeles, CA",
+            "dates": "November 2012 - March 2017",
+            "description": "lorem ipsum"
         }
     ]
+};
+
+function locationizer(work_obj) {
+    var locationArray = [];
+
+    for(job in work_obj.jobs) {
+        var newLocation = work_obj.jobs[job].location;
+        locationArray.push(newLocation);
+    }
+
+    return locationArray;
+
 }
+
+// Did locationizer() work? This line will tell you!
+console.log(locationizer(work));
 
 var education = {
     "schools": [
@@ -73,12 +87,59 @@ var projects = {
             "dates": "2017",
             "description": "lorem ipsum",
             "images": [
-                "/images/pic.png",
-                "/images/pic.png"
+                "/images/197x148.gif",
+                "/images/197x148.gif"
             ]
         }
     ]
-}
+};
 
+if (bio.skills.length > 0) {
+    $("#header").append(HTMLskillsStart);
+    var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
+    $("#skills").append(formattedSkill);
+    formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
+    $("#skills").append(formattedSkill);
+}
 $("#main").append(work["position"]);
-$("#main").append(education.name);
+$("#main").append(education.schools);
+
+function displayWork() {
+    for (job = 0; job < work.jobs.length; job++) {
+        $("#workExperience").append(HTMLworkStart);
+
+        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+        var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+        var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+        var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+        var formattedAll = formattedEmployer + formattedTitle + formattedDates + formattedDescription;
+
+        $(".work-entry:last").append(formattedAll);
+    }
+}
+displayWork();
+
+projects.display = function() {
+    for(project in projects.projects) {
+        $("#projects").append(HTMLprojectStart);
+
+        var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+        $(".project-entry:last").append(formattedTitle);
+
+        var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+        $(".project-entry:last").append(formattedDates);
+
+        var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+        $(".project-entry:last").append(formattedDescription);
+
+        if(projects.projects[project].images.length > 0) {
+            for(image in projects.projects[project].images) {
+                var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+                $(".project-entry:last").append(formattedImage);
+            }
+        }
+    }
+}
+projects.display();
+
+$("#mapDiv").append(googleMap);
